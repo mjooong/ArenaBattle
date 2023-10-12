d60ab42cfa7fddcfdccfa9ee323102889dff19d1
MinjongKim <minjong0524@gmail.com>
2023-10-12 21:14:44 +0900

    게임 구성 요소를 폴더별로 분류, 헤더 참조를 최소화하는 규칙 수립

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+//#include "Player/ABPlayerController.h"
+
+AABGameMode::AABGameMode()
+{
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Script/Engine.Blueprint'/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C'"));
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+	//DefaultPawnClass
+
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerRef(TEXT("/Script/CoreUObject.Class'/Script/ArenaBattle.ABPlayerController'"));
+	if (PlayerControllerRef.Class)
+	{
+		PlayerControllerClass = PlayerControllerRef.Class;
+	}
+
+	//PlayerControllerClass = AABPlayerController::StaticClass();
+
+
+
+}
---
+++ Source/ArenaBattle/Player/ABPlayerController.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/ABPlayerController.h"
+
+void AABPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/** 시작시 포커스가 뷰포트 안으로 들어감*/
+	FInputModeGameOnly GameOnlyInputMode;
+	SetInputMode(GameOnlyInputMode);
+
+}
